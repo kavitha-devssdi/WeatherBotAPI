cac4de78da4fc673b823775a402ca0a715b7a90c
Kavitha G <gkavitha@ssdiblr.sharp.co.in>
2026-02-16 11:43:24 +0530

    Added OpenAPI servers section

--- Program.cs
+++ Program.cs
@@ -3,7 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddServer(new Microsoft.OpenApi.Models.OpenApiServer
+    {
+        Url = "https://weatherbotapi-oxsv.onrender.com"
+    });
+});
+
 builder.Services.AddHttpClient();
 
 
